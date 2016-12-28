8ce17a6f12ee7a3dee666b1ce62f7d0e2c7e5ff7
Steven Yang <sgyang9@gmail.com>
2016-12-27 22:14:40 -0500

    verification token edit

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
 	}
-	res.send('Error, wrong token')
+	else {
+		res.send('Error, wrong token')
+	}
 })
 
 // to post data
